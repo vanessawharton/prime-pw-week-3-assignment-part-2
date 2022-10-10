1cca4c590fcb4f192617530633082881a36bbadd
Vanessa Wharton <vanessa.primetime@gmail.com>
2022-10-09 23:19:56 -0500

    Removed endless loop

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -59,7 +59,7 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 while(stars){
-  console.log(stars);
+  console.log();
 }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
